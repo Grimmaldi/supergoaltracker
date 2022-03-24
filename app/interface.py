a703984026b1d097e05ba06a0919ed8137902241
Chen Feldman <ccfeldman@gmail.com>
2022-03-24 06:49:29 -0400

    Updating structure and adding functionality

---
+++ app/interface.py
@@ -0,0 +1,22 @@
+from urllib import response
+
+
+def welcome():
+    print('Welcome to SuperGoal Tracker!')
+
+def new_user():
+    username = input('Please provide a username.\n')
+    activities = add_activity_form()
+    return username, activities
+
+def add_activity_form():
+    activities = []
+    while True:
+        response = input('Do you have an activity that you would like to add? [Yes/No]').lower().trim()
+        if response == 'no':
+            break
+        elif response == 'yes':
+            activity_name = 'What is the name of the that you wish to track?'
+            activities.append(activity_name)
+        else:
+            print('Sorry, I do not understand.  Please respond either "yes" or "no".\n')
--- app/model.py
+++ app/model.py
@@ -8,15 +8,30 @@ class User:
         return f"User({self.name})"
 
     def add_activity(self, activity_name, **kwargs):
-        self.activities.append(Activity(activity_name, **kwargs))
+        if activity_name not in self:
+            self.activities.append(Activity(activity_name, **kwargs))
+        else:
+            print('Activity previously added.')
 
     def delete_activity(self, activity_name):
+        if activity_name in self:
+            for i, activity in enumerate(self.activities):
+                if activity_name == activity.activity_name:
+                    del self.activities[i]
+                    break
+        else:
+            print('Activity not found.')
+
+    def edit_activity(self, activity_name, metric, new_value):
         for i, activity in enumerate(self.activities):
-            if activity.activity_name == activity_name:
-                del self.activities[i]
-                break
+            if activity_name == activity.activity_name:
+                activity[i].edit(metric, new_value)
 
-
+    def __contains__(self, activity_name):
+        for activity in self.activities:
+            if activity_name == activity.activity_name:
+                return True
+        return False
 
 class Activity:
 
